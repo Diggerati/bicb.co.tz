c7fb5ab633ab45e069253051ef9b4c73891650a8
Diggerati <innocentmasuki99@gmail.com>
2020-04-15 04:51:38 +0300

    added some more appearances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule    
+    MatButtonModule,
+    MatCardModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
