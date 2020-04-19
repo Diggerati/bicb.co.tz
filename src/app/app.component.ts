c6eb6fbe859e7c69f155333c27cb2cc313d4f5d7
Diggerati <innocentmasuki99@gmail.com>
2020-04-19 19:42:55 +0300

    added mobile app component for responsove styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              './app-responsive.component.css']
 })
 export class AppComponent {
   title = 'BICB | Microfinance';
