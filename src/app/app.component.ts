007e6a5f6e081150bf7eb3dc13468a534bce6db7
Diggerati <innocentmasuki99@gmail.com>
2020-04-26 17:51:54 +0300

    added some social media optimization for the SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,14 @@ export class AppComponent {
       { name: 'description', content: 'Financial institution operating in Tanzania providing loans to small entrepreneurs at low rates.' },
       // { name: 'robots', content: 'index, follow' },
       { name: 'author', content: 'Innocent Masuki' },
+      { property: 'og:title', content: 'BICB | Microfinance' },
+      { property: "og:url", content: "https://bicb.co.tz/" },
       { property: "og:description", content:"Financial institution operating in Tanzania providing loans to small entrepreneurs at low rates."},
       { property: "og:image", content: "https://www.bicb.co.tz/covers/B_logo.png" },
       { name: "twitter:description", content:"Financial institution operating in Tanzania providing loans to small entrepreneurs at low rates."},
-      {name:"twitter:image", content: "https://www.bicb.co.tz/covers/B_logo.png"}
+      { name:"twitter:image", content: "https://www.bicb.co.tz/covers/B_logo.png"},
+      { name:"twitter:site", content:"@bicb_finance"},
+      { name:"twitter:creator", content: "@MasukiInnocent"}
     ]);
   }
 }
